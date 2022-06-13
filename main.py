45f055755f68c182257d3197bc9c8667c6112429
Toxa124 <good12456@mail.ru>
2022-06-13 21:28:46 +0300

    Добавлено определение части речи для отдельных слов

--- main.py
+++ main.py
@@ -1,4 +1,20 @@
 with open("words.txt", "r",encoding="utf-8") as f:
    string=  f.read()
    string=string.split()
-   print(string)
+
+def inRu(enWord):
+   switch = {"VERB": "Глагол",
+             "NOUN": "Существительное",
+             "PREP": "Предлог",
+             "INTJ": "Междометие"}
+   return switch[enWord]
+
+import pymorphy2
+morph = pymorphy2.MorphAnalyzer()
+
+for i,word in enumerate(string):
+    a=morph.parse(word)[0]
+    string[i] = word + " = " + inRu(a.tag.POS)
+
+for a in string:
+    print(a)
