b10bc052939d2ab28378da58731bbdc07c5452d3
Toxa124 <good12456@mail.ru>
2022-06-14 01:02:18 +0300

    fix typography

--- main.py
+++ main.py
@@ -2,19 +2,19 @@ with open("words.txt", "r",encoding="utf-8") as f:
    string=  f.read()
    string=string.split()
 
-def inRu(enWord):
+def in_ru(en_word):
    switch = {"VERB": "Глагол",
              "NOUN": "Существительное",
              "PREP": "Предлог",
              "INTJ": "Междометие"}
-   return switch[enWord]
+   return switch[en_word]
 
 import pymorphy2
 morph = pymorphy2.MorphAnalyzer()
 
 for i,word in enumerate(string):
     a=morph.parse(word)[0]
-    string[i] = word + " = " + inRu(a.tag.POS)
+    string[i] = word + " = " + in_ru(a.tag.POS)
 
 for a in string:
     print(a)
